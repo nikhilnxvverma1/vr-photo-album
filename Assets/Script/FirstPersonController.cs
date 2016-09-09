4c20a79d7354160b155b21ce3d2ca1472fc5213c
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-09-09 07:56:19 -0400

    Implemented a basic first person controller character

---
+++ Assets/Script/FirstPersonController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+
+	public float speed=10f;
+
+	// Use this for initialization
+	void Start () {
+		//lock the cursor's position
+		Cursor.lockState=CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float translation=Input.GetAxis("Vertical") * speed;
+		float strafe=Input.GetAxis("Horizontal") *speed;
+		translation *=Time.deltaTime; 
+		strafe *=Time.deltaTime; 
+
+		transform.Translate(strafe,0,translation);
+
+		//resume moving the cursor when user presses escape
+		if(Input.GetKeyDown("escape")){
+			Cursor.lockState=CursorLockMode.None;
+		}
+	
+	}
+}
---
+++ Assets/Script/LookAround.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAround : MonoBehaviour {
+
+	Vector2 mouseLook;
+	Vector2 smoothV;
+	public float senstivity=5.0f;
+	public float smoothing=2.0f;
+
+	GameObject charecter;
+
+	// Use this for initialization
+	void Start () {
+		charecter=this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction=new Vector2(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y"));
+		direction=Vector2.Scale(direction,new Vector2(senstivity*smoothing,senstivity*smoothing));
+
+		smoothV.x=Mathf.Lerp(smoothV.x,direction.x,1f/smoothing);
+		smoothV.y=Mathf.Lerp(smoothV.y,direction.y,1f/smoothing);
+		mouseLook+=smoothV;
+
+		transform.localRotation=Quaternion.AngleAxis(-mouseLook.y,Vector3.right);
+		charecter.transform.localRotation=Quaternion.AngleAxis(mouseLook.x,charecter.transform.up);
+	}
+}
