faa99b3b0e36d2027b137b68dbcbdd63e8ee08f9
Mychal <mychallipson@gmail.com>
2016-10-07 16:36:02 -0400

    Added planes to planetarium
    Split plane generation into 2 scripts, one for generating and moving the
    planes, one for generating the pictures for a single plane. Added a game
    object with the generation script to the planetarium scene

---
+++ Assets/CreatePlanes.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CreatePlanes : MonoBehaviour {
+
+	// Use this for initialization
+	public float numPlanes, initialHeight, separation, planescale;
+	private List<GameObject> planes;
+	void Start () {
+		planes = new List<GameObject>();
+		for (int i = 0; i < numPlanes; i++)
+		{
+			GameObject plane = new GameObject();
+			plane.transform.parent = this.transform;
+			plane.transform.localPosition = new Vector3(0, initialHeight + i * separation, 0);
+			var curPlane = plane.AddComponent<CreatePhotoPlane>();
+			curPlane.planescale = planescale;
+			planes.Add(plane);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject p in planes)
+		{
+			
+			if(p.transform.position.y-1 <= initialHeight)
+			{
+				p.transform.Translate(0, numPlanes*separation-1, 0);
+			}else
+			{
+				p.transform.Translate(0, -1*Time.deltaTime/2, 0);
+			}
+		}
+	}
+}
--- Assets/Script/CreatePhotoPlane.cs
+++ Assets/Script/CreatePhotoPlane.cs
@@ -4,20 +4,21 @@ using System.Collections.Generic;
 public class CreatePhotoPlane : MonoBehaviour {
 
 	// Use this for initialization
-	public float planex, planey, planez, planescale;
+	public float planescale;
 	void Start () {
-		byte[] image = System.IO.File.ReadAllBytes(System.IO.Directory.GetCurrentDirectory() + @"/Assets/Starsinthesky.jpg");
+		var slash = System.IO.Path.DirectorySeparatorChar;
+		byte[] image = System.IO.File.ReadAllBytes(System.IO.Directory.GetCurrentDirectory() + string.Format(@"{0}Assets{1}Starsinthesky.jpg",slash,slash));
 		Texture2D t2 = new Texture2D(3877, 2842);
 		t2.LoadImage(image);
-		planex = 0; planey = 0; planez = 0; planescale = 5;
 		List<GameObject> photos = new List<GameObject>();
 		System.Random r = new System.Random();
 		for (int i = 0; i < 10; i++)
 		{
 			GameObject photo = GameObject.CreatePrimitive(PrimitiveType.Quad);
+			photo.transform.parent = this.transform;
 			float px = (float)r.NextDouble() * planescale*planescale - planescale*planescale/2;
 			float pz = (float)r.NextDouble() * planescale*planescale - planescale*planescale/2;
-			photo.transform.position = new Vector3(px, planey, pz);
+			photo.transform.localPosition = new Vector3(px, 0, pz);
 			photo.transform.eulerAngles = new Vector3(-90, 0, 0);
 
 			Material mat = new Material(Shader.Find("Unlit/Texture"));
