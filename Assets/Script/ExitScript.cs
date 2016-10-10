b3589c4c8c62e0309463487db3f675927373aa7f
mychallipson <mychallipson@gmail.com>
2016-10-10 17:17:10 -0400

    Added exit script and script to attach to doors to specify where to exit to

---
+++ Assets/Script/ExitScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ExitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            GameObject hitObject = hitInfo.transform.root.gameObject;
+            if(hitObject.tag == "Door" && Input.GetKeyDown(KeyCode.Return))
+            {
+                var j2s = hitObject.GetComponent<JumpToScene>();
+                SceneManager.LoadScene(j2s.scene);
+            }
+            
+
+            
+
+        }
+
+
+    }
+}
