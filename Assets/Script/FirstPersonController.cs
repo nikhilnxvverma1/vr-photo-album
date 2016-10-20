1fe98dada7237eb3f6b1ca198b32e5b47a028124
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-10-20 11:32:24 -0400

    Removed hardcoded input for player movement.
    Couldn't see any side effects as far as the strafing issue goes.

--- Assets/Script/FirstPersonController.cs
+++ Assets/Script/FirstPersonController.cs
@@ -14,26 +14,26 @@ public class FirstPersonController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-//		float translation=Input.GetAxis("Vertical") * speed;
-//		float strafe=Input.GetAxis("Horizontal") *speed;
-//		translation *=Time.deltaTime; 
-//		strafe *=Time.deltaTime; 
-//
-//		transform.Translate(strafe,0,translation);
+		float translation=Input.GetAxis("Vertical") * speed;
+		float strafe=Input.GetAxis("Horizontal") *speed;
+		translation *=Time.deltaTime; 
+		strafe *=Time.deltaTime; 
 
+		transform.Translate(strafe,0,translation);
 
-		if(Input.GetKey(KeyCode.W)) {
-			transform.position += Camera.main.transform.forward * Time.deltaTime * speed;
-		}
-		else if(Input.GetKey(KeyCode.S)) {
-			transform.position += -Camera.main.transform.forward * Time.deltaTime * speed;
-		}
-		else if(Input.GetKey(KeyCode.A)) {
-			transform.position += Vector3.left * Time.deltaTime * speed;
-		}
-		else if(Input.GetKey(KeyCode.D)) {
-			transform.position += Vector3.right * Time.deltaTime * speed;
-		}
+		// this will get discussed if it is actually needed
+		// if(Input.GetKey(KeyCode.W)) {
+		// 	transform.position += Camera.main.transform.forward * Time.deltaTime * speed;
+		// }
+		// else if(Input.GetKey(KeyCode.S)) {
+		// 	transform.position += -Camera.main.transform.forward * Time.deltaTime * speed;
+		// }
+		// else if(Input.GetKey(KeyCode.A)) {
+		// 	transform.position += Vector3.left * Time.deltaTime * speed;
+		// }
+		// else if(Input.GetKey(KeyCode.D)) {
+		// 	transform.position += Vector3.right * Time.deltaTime * speed;
+		// }
 		//resume moving the cursor when user presses escape
 		if(Input.GetKeyDown("escape")){
 			Cursor.lockState=CursorLockMode.None;
