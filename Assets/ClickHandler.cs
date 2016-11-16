014608cf75a504f8715a0f8f4269aeb074b87b03
mychallipson <mychallipson@gmail.com>
2016-11-16 18:10:10 -0500

    Press enter/a button to leave tutorial enabled

--- Assets/ClickHandler.cs
+++ Assets/ClickHandler.cs
@@ -4,11 +4,9 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class ClickHandler : MonoBehaviour {
-	public Button myButton;
 	// Use this for initialization
 	void Start () {
-		Button btn = myButton.GetComponent<Button>();
-		btn.onClick.AddListener(LoadOnClick);
+
 	}
 	
 	void LoadOnClick()
@@ -17,6 +15,18 @@ public class ClickHandler : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+        else if (DataScan.OS == DataScan.OS_TYPE.MAC && Input.GetKeyDown("joystick button 16"))
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+        else if (Input.GetKeyDown("joystick button 0"))
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+    }
 }
