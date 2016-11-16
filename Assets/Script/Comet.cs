bba3abfbe928764e37e8a067a2c5b7a9703424f5
xz9876 <xz9876@g.rit.edu>
2016-11-16 16:23:17 -0500

    decoration of the planetarium

---
+++ Assets/Script/Comet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Comet : MonoBehaviour {
+	public float speed=20f;
+	public float x;
+	public float y;
+	public float z;
+	public float length;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		length = speed * Time.deltaTime;
+		if (x > 20) {
+			x = Random.Range(-20, 20);
+			y = Random.Range(10, 17);
+			z = Random.Range(-10, -7);
+		}
+		x += length;
+		transform.position = new Vector3(x,y,z);
+	}
+}
