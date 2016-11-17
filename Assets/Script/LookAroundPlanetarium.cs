a44f637bcfa71253b9a766bdf90609bb68697604
mychallipson <mychallipson@gmail.com>
2016-11-17 18:23:35 -0500

    Fixed up planetarium issues

---
+++ Assets/Script/LookAroundPlanetarium.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAroundPlanetarium : MonoBehaviour {
+
+	Vector2 mouseLook;
+	Vector2 smoothV;
+	public float senstivity=5.0f;
+	public float smoothing=2.0f;
+
+	GameObject charecter;
+
+	// Use this for initialization
+	void Start () {
+		charecter=this.transform.parent.gameObject;
+        mouseLook = new Vector2(1, 30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction=new Vector2(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y"));
+		direction=Vector2.Scale(direction,new Vector2(senstivity*smoothing,senstivity*smoothing));
+
+		smoothV.x=Mathf.Lerp(smoothV.x,direction.x,1f/smoothing);
+		smoothV.y=Mathf.Lerp(smoothV.y,direction.y,1f/smoothing);
+		mouseLook+=smoothV;
+
+		transform.localRotation=Quaternion.AngleAxis(-mouseLook.y,Vector3.right);
+		charecter.transform.localRotation=Quaternion.AngleAxis(mouseLook.x,charecter.transform.up);
+
+		if (DataScan.OS == DataScan.OS_TYPE.MAC) {
+			direction = new Vector2 (Input.GetAxis ("Horizontal_R_Mac"), Input.GetAxis ("Vertical_R_Mac"));
+		} else {
+			direction = new Vector2 (Input.GetAxis ("Horizontal_R_Win"), Input.GetAxis ("Vertical_R_Win"));
+		}
+		direction = Vector2.Scale (direction, new Vector2 (senstivity * smoothing, senstivity * smoothing));
+
+		smoothV.x = Mathf.Lerp (smoothV.x, direction.x, 1f / smoothing);
+		smoothV.y = Mathf.Lerp (smoothV.y, direction.y, 1f / smoothing);
+		mouseLook += smoothV;
+
+		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
+		charecter.transform.localRotation = Quaternion.AngleAxis (mouseLook.x, charecter.transform.up);
+	}
+}
