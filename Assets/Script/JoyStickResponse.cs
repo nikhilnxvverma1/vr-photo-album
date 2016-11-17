2975004b5c3446d9c1287301efb87587908eb767
arjun-dhuliya <arjun.dhuliya@gmail.com>
2016-11-16 21:56:45 -0500

    Added JoyStickResponse Script for Back Button to all scenes

---
+++ Assets/Script/JoyStickResponse.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class JoyStickResponse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape) || 
+			(DataScan.OS == DataScan.OS_TYPE.WINDOWS && Input.GetKeyDown ("joystick button 6")) ||
+			(DataScan.OS == DataScan.OS_TYPE.MAC && Input.GetKeyDown ("joystick button 10"))){
+			Debug.Log ("Back Button detected Taking to Tutorial Scene");
+			DataScan.currentAlbum = DataScan.rootModel.albumList [0];
+			SceneManager.LoadScene ("Tutorial");
+		} 		
+	}
+}
